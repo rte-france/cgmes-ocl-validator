77c5569c2b0a7c54e953322a1bf8b0de2f1e6563
Jerome Picault <jerome.picault@rte-france.com>
2020-05-07 16:36:27 +0200

    Increased version number

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -2,5 +2,5 @@
 INPUTS=inputs
 export VALIDATOR_HOME=.
 export VALIDATOR_CONFIG=config
-java -Xms1g -cp target/ocl_validator-2.0-jar-with-all-dependencies.jar ocl.OCLEvaluator $INPUTS
+java -Xms1g -cp target/ocl_validator-2.1-jar-with-all-dependencies.jar ocl.OCLEvaluator $INPUTS
 
