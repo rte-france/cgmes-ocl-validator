0a4a5e82c31c70fea7ee4195f18119c796e9e273
Jerome Picault <jerome.picault@rte-france.com>
2020-07-29 10:11:17 +0200

    see #28: missing scripts

---
+++ scripts/startValidationDaemon.bat
@@ -0,0 +1,7 @@
+set VALIDATOR_INPUTS=inputs
+set VALIDATOR_OUTPUTS=reports
+set VALIDATOR_HOME=.
+set VALIDATOR_CONFIG=config
+chcp 65001
+java -Xms1g -cp target/ocl_validator-3.0-jar-with-all-dependencies.jar ocl.CGMESValidatorDaemon
+PAUSE
